c5ceebe7f358481eb9a03c7dd63d5f943338c7f7
bensammwaniki <bensammwaniki@gmail.com>
2021-09-07 23:36:13 +0300

    function to hundle users  input

--- user.py
+++ user.py
@@ -2,4 +2,15 @@ class User:
     '''
     initialize class user
     '''
-user = []
+    user = []
+    def __init__(self,username,password):
+        '''
+        Args:
+        username(string)
+        password(string)
+
+        '''
+        self.username=username
+        self.password=password
+
+        pass
