cc9fc008143819b7d7498cc878ed995807e5ee2c
bensammwaniki <bensammwaniki@gmail.com>
2021-09-08 15:22:20 +0300

    generate random password

--- user.py
+++ user.py
@@ -1,4 +1,7 @@
+import pyperclip
+import string
 
+import random
 class User:
     '''
     initialize class user
@@ -75,4 +78,30 @@ class Credentials():
         for match in show.credentials_list:
             if match.account == account:
                 return match   
-                
+    @classmethod
+    def copy_password(show,account):
+        matched = Credentials.searchcredential(account)
+        pyperclip.copy(matched.password)     
+    @classmethod
+    def credential_exist(show, account):
+        """
+        Method that checks if a credential exists.
+        """
+        for credential in show.credentials_list:
+            if credential.account == account:
+                return True
+            else:
+             return False     
+
+    @classmethod
+    def display_credentials(cls):
+        """
+        Method that returns all items in the credentials list
+        """
+        return cls.credentials_list
+
+    def generatePassword(stringLength=8):
+        """Generate a random password string of letters and digits and special characters"""
+        password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "~!@#$%^&*"
+        return ''.join(random.choice(password) for i in range(stringLength))
+              
